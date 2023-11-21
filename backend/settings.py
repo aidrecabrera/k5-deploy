f919d18a019b1f6f567718058ab75d79793bb29c
Svene <aidrecabrera@gmail.com>
2023-11-21 08:08:10 +0800

    update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -88,7 +88,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
