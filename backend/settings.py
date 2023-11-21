e2b5ba4175015e5e5181a95da6305a44511add6e
Svene <aidrecabrera@gmail.com>
2023-11-21 08:09:18 +0800

    update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -89,6 +89,4 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
